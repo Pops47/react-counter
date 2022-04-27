e459e2d514bc9542be5113c94be9bbd8934156ee
Pauline Soubrié <pauline_soubrie@hotmail.fr>
2022-04-27 22:59:39 +0200

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import logo from "./assets/images/logo-calculatrice.png";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <img className="logo" src={logo} alt="logo calculatrice" />
+        <span>React Counter</span>
       </header>
+      <main className="main">
+        <div className="top">
+          <button
+            onClick={() => {
+              setCounter(counter - 1);
+            }}
+          >
+            {counter === 0 ? "" : "-"}
+          </button>
+          <p className="blue-bgc">{counter}</p>
+          <button
+            onClick={() => {
+              setCounter(counter + 1);
+            }}
+          >
+            {counter === 10 ? "" : "+"}
+          </button>
+        </div>
+        <button
+          className="blue-bgc"
+          onClick={() => {
+            setCounter(0);
+          }}
+        >
+          Reset
+        </button>
+      </main>
+      <footer className="footer">
+        Made with <span>React</span> at <span>Le Reacteur</span> by{" "}
+        <span>Pops</span>
+      </footer>
     </div>
   );
 }
